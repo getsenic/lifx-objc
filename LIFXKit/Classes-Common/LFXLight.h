137b44582c6dca3a061a36b69b9269f4ec13c731
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2016-09-05 17:28:37 +0200

    Mark light properties with nullable annotations

--- LIFXKit/Classes-Common/LFXLight.h
+++ LIFXKit/Classes-Common/LFXLight.h
@@ -16,37 +16,37 @@
 @interface LFXLight : LFXDevice <LFXLightTarget>
 
 // Tags
-@property (nonatomic, readonly) NSArray /* NSString */ *tags;
-@property (nonatomic, readonly) NSArray /* LFXTaggedLightCollection */ *taggedCollections;
+@property (nonatomic, readonly) NSArray /* NSString */ * _Nonnull tags;
+@property (nonatomic, readonly) NSArray /* LFXTaggedLightCollection */ * _Nonnull taggedCollections;
 
 
 // Most of these Light State and Light Control methods are declared in LFXLightTarget,
 // so you can use the same methods on both LFXLight and LFXLightCollection objects.
 
 // Light State
-- (NSString *)label;
-- (LFXHSBKColor *)color;
+- (NSString * _Nullable)label;
+- (LFXHSBKColor * _Nullable)color;
 - (LFXPowerState)powerState;
 - (LFXFuzzyPowerState)fuzzyPowerState;
 
 
 // Light Control
-- (void)setLabel:(NSString *)label;
-- (void)setColor:(LFXHSBKColor *)color;
-- (void)setColor:(LFXHSBKColor *)color overDuration:(NSTimeInterval)duration;
+- (void)setLabel:(NSString * _Nonnull)label;
+- (void)setColor:(LFXHSBKColor * _Nonnull)color;
+- (void)setColor:(LFXHSBKColor * _Nonnull)color overDuration:(NSTimeInterval)duration;
 - (void)setPowerState:(LFXPowerState)powerState;
 
 
 // Firmware Versions
-- (NSString *)meshFirmwareVersion;
-- (NSString *)wifiFirmwareVersion;
-- (BOOL)meshFirmwareVersionIsAtLeast:(NSString *)version;
-- (BOOL)wifiFirmwareVersionIsAtLeast:(NSString *)version;
+- (NSString * __nullable)meshFirmwareVersion;
+- (NSString * __nullable)wifiFirmwareVersion;
+- (BOOL)meshFirmwareVersionIsAtLeast:(NSString * __nonnull)version;
+- (BOOL)wifiFirmwareVersionIsAtLeast:(NSString * __nonnull)version;
 
 
 // Observers
-- (void)addLightObserver:(id <LFXLightObserver>)observer;
-- (void)removeLightObserver:(id <LFXLightObserver>)observer;
+- (void)addLightObserver:(id <LFXLightObserver> __nonnull)observer;
+- (void)removeLightObserver:(id <LFXLightObserver> __nonnull)observer;
 
 @end
 
@@ -54,13 +54,13 @@
 @protocol LFXLightObserver <NSObject>
 
 @optional
-- (void)light:(LFXLight *)light didChangeLabel:(NSString *)label;
-- (void)light:(LFXLight *)light didChangeColor:(LFXHSBKColor *)color;
-- (void)light:(LFXLight *)light didChangePowerState:(LFXPowerState)powerState;
-- (void)light:(LFXLight *)light didChangeReachability:(LFXDeviceReachability)reachability;
+- (void)light:(LFXLight * _Nonnull)light didChangeLabel:(NSString * _Nullable)label;
+- (void)light:(LFXLight * _Nonnull)light didChangeColor:(LFXHSBKColor * _Nullable)color;
+- (void)light:(LFXLight * _Nonnull)light didChangePowerState:(LFXPowerState)powerState;
+- (void)light:(LFXLight * _Nonnull)light didChangeReachability:(LFXDeviceReachability)reachability;
 
-- (void)light:(LFXLight *)light didChangeMeshFirmwareVersion:(NSString *)meshFirmwareVersion;
-- (void)light:(LFXLight *)light didChangeWifiFirmwareVersion:(NSString *)wifiFirmwareVersion;
+- (void)light:(LFXLight * _Nonnull)light didChangeMeshFirmwareVersion:(NSString * _Nullable)meshFirmwareVersion;
+- (void)light:(LFXLight * _Nonnull)light didChangeWifiFirmwareVersion:(NSString * _Nullable)wifiFirmwareVersion;
 
 @end
 
